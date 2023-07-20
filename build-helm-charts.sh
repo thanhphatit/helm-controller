5449731545c03c7043ad2e575a1b3a2c6125fb0d
root <thanhphatit95@gmail.com>
2023-07-20 08:14:23 +0000

    Update time: 20230720-081420

--- build-helm-charts.sh
+++ build-helm-charts.sh
@@ -483,20 +483,14 @@ function build_helm_charts(){
             elif [[ "${METHOD}" == "acr" ]];then
                 check_var "ACR_NAME AZ_USER AZ_PASSWORD"
                 pre_check_dependencies "az"
+
                 if [[ $(helm push ${PACKAGE_PATH} ${ACR_ARTIFACT_NAME}) ]];then
-                    echo "Upload"
-                else
-                    echo "khong upload"
+                    if [[ $(cat ${TMPFILE_CHART_INFO_REPO} | wc -l) -ne 0 ]];then
+                        az acr helm push --force -n ${ACR_NAME} -u ${AZ_USER} -p ${AZ_PASSWORD} ${PACKAGE_PATH}
+                    else
+                        az acr helm push -n ${ACR_NAME} -u ${AZ_USER} -p ${AZ_PASSWORD} ${PACKAGE_PATH}
+                    fi
                 fi
-                # if [[ $(cat ${TMPFILE_CHART_INFO_REPO} | wc -l) -ne 0 ]];then
-                #     #helm push --force ${PACKAGE_PATH} ${ACR_ARTIFACT_NAME}
-                #     #az acr helm push --force -n ${ACR_NAME} -u ${AZ_USER} -p ${AZ_PASSWORD} ${PACKAGE_PATH}
-                #     helm push --force ${PACKAGE_PATH} ${ACR_ARTIFACT_NAME}
-                # else
-                #     helm push ${PACKAGE_PATH} ${ACR_ARTIFACT_NAME}
-                #     #helm push ${PACKAGE_PATH} ${ACR_ARTIFACT_NAME}
-                #     #az acr helm push -n ${ACR_NAME} -u ${AZ_USER} -p ${AZ_PASSWORD} ${PACKAGE_PATH}
-                # fi
             fi
 
             echo ""
